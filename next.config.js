0b317f44d87a280fc994b87089eae0630895df50
serin4103 <serin4103@gmail.com>
2025-10-31 01:34:31 +0900

    add: GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -4,15 +4,15 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     esmExternals: false
+  },
+  // GitHub Pages 배포 설정
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/kweb-static',
+  assetPrefix: '/kweb-static/',
+  images: {
+    unoptimized: true
   }
-  // GitHub Pages 배포 설정 (배포시 주석 해제)
-  // output: 'export',
-  // trailingSlash: true,
-  // basePath: '/kweb-view',
-  // assetPrefix: '/kweb-view/',
-  // images: {
-  //   unoptimized: true
-  // }
 }
 
 module.exports = nextConfig
